305e8ab0d4a5e0de537057bba2bb399478a6b7e7
felipecabesssa <felipesoares100@hotmail.com>
2022-04-02 12:38:41 -0300

    Projeto criado

---
+++ src/main/java/br/com/produtos/apirestbackend/ApirestbackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.produtos.apirestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/produtos/apirestbackend/ApirestbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.produtos.apirestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
